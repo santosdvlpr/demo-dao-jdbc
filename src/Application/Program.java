3539e5421ea862520e53146bb102a38b6f967b90
Humberto S Pinto <humbdsp@hotmail.com>
2020-04-18 10:26:34 -0300

    SellerDaoJdbc and DaoFactory

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -2,6 +2,8 @@ package Application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.*;
 public class Program {
 
@@ -9,6 +11,9 @@ public class Program {
 		
 		Department obj1 = new Department(1,"Books");
 		Seller obj2 = new Seller(1,"Henzo P Santana","henzo@hotmail.com",new Date(),8000.00,obj1);
+
+		SellerDao sellerDao = DaoFactory.createSellerDao(); 
+		
 		System.out.println(obj2);
 
 	}
